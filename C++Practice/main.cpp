71128c5a94f876115289ac732a5c6518a6a24b71
Alam Figueroa <alamx.11@gmail.com>
2021-04-18 16:46:54 -0700

    Added 'Longest Substring Without Repeating Characters' function

---
+++ C++Practice/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  C++Practice
+//
+//  Created by Alam Figueroa Aguilar on 4/18/21.
+//
+
+#include <iostream>
+
+#include "ArraySolutions.h"
+
+int main(int argc, const char * argv[]) {
+    ArraySolutions arrayExercises;
+    
+    cout << arrayExercises.lengthOfLongestSubstring("abcabcbb") << endl;
+    return 0;
+}
